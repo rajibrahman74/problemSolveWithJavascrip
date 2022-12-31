a18b8470ef58e66e7d603c178412f0c49ca8aba7
rajibrahman74e <rajiburrahman74@gmail.com>
2022-12-31 08:14:07 +0600

    done

---
+++ GradingSystem/script.js
@@ -0,0 +1,38 @@
+// If you want to execute this code, please comment out it first. step by step
+// How to calculate grade in javascript function
+// function declared here,
+
+// function getGrad(mark) {
+//   let latterGrad = " ";
+//   if (mark >= 80 && mark <= 100) {
+//     latterGrad = "A+";
+//     console.log("You are passed");
+//   } else if (mark >= 70 && mark < 80) {
+//     latterGrad = "A";
+//     console.log("You are passed");
+//   } else if (mark >= 60 && mark < 70) {
+//     latterGrad = "A-";
+//     console.log("You are passed");
+//   } else if (mark >= 50 && mark < 60) {
+//     latterGrad = "B";
+//     console.log("You are passed");
+//   } else if (mark >= 40 && mark < 50) {
+//     latterGrad = "C";
+//     console.log("You are passed");
+//   } else if (mark >= 33 && mark < 40) {
+//     latterGrad = "D";
+//     console.log("You are passed");
+//   } else if (mark >= 0 && mark < 33) {
+//     latterGrad = "F";
+//     console.log("Your are faild try and again later");
+//   } else {
+//     console.log("Your Result is not found");
+//     latterGrad = "Nothing found";
+//   }
+//   return "You Got: " + latterGrad;
+// }
+
+// input your mark here,
+
+// let input = getGrad(32);
+// console.log(input);
