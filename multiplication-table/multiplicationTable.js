9e5168c6870e6d4d31db5882d7d289ad6492e40a
rajibrahman74e <rajiburrahman74@gmail.com>
2023-01-30 20:47:07 +0600

    [ADD]-multiplicationTable

---
+++ multiplication-table/multiplicationTable.js
@@ -0,0 +1,14 @@
+/* If you want to execute this code, please comment out it first. step by step
+How to calculate grade in javascript function
+function declared here, */
+
+/*
+function multiplicationTable(inputNumber) {
+  for (let i = 1; i <= 10; i++) {
+    let result = i * inputNumber;
+    console.log(result);
+  }
+}
+
+multiplicationTable(13);
+*/
